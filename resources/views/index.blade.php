3edeaf1a149f5cd1d42e0a0d6e5049245473f391
MauroBorzi <mauroborzi91@gmail.com>
2025-11-11 11:39:32 +0100

    Implement TrainController and update routes to display train data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>laravel migration</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>ciao</h1>
-            </div>
+            <div class="col-12"><h1>TRENI</h1></div>
+           @foreach ($collection as $item)
+               
+           @endforeach
         </div>
     </div> 
 </body>
